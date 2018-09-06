3a3183c10be0a34e45b90fa166e67e3d8458c6df
sjistheboss <sjistheboss@gmail.com>
2018-09-06 13:37:18 +0600

    Add files via upload

---
+++ small_bot_with_IR/small_bot_with_IR.ino
@@ -0,0 +1,107 @@
+
+int lm1 = 2;
+int lm2 = 3;
+int rm1 = 4;
+int rm2 = 5;
+int sensorPin = A0;
+int sensorData;
+boolean calibration, frizz;
+int blackThreshold, whiteThreshold;
+int mainThreshold;
+
+void setup() {
+
+  pinMode(lm1, OUTPUT);
+  pinMode(lm2, OUTPUT);
+  pinMode(rm1, OUTPUT);
+  pinMode(rm2, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  while (Serial.available() == 0 ) {
+    //Serial.println(analogRead(sensorPin));
+  }
+
+
+  char val = Serial.read() ;//reads the signal
+  //Serial.print(val);
+  if (val == 'b') {
+    blackThreshold  = analogRead(sensorPin);
+    Serial.println(blackThreshold);
+
+  }
+  if (val == 'w') {
+    whiteThreshold = analogRead(sensorPin);
+    Serial.println(whiteThreshold);
+  }
+
+  if (val == 's') {
+    if ( blackThreshold > 0  && whiteThreshold > 0) {
+      calibration = true;
+      int diff = blackThreshold - whiteThreshold;
+      mainThreshold = blackThreshold - (diff / 5);
+      Serial.println(mainThreshold);
+    }
+  }
+  if (vals == 'u') {
+    frizz = false;
+  }
+
+  
+  if (calibration && !frizz) {
+    sensorState();
+
+    /*********For Forward *********/
+    if (val == 'F') {
+      Serial.println("FORWARD");
+      digitalWrite(lm1, HIGH);      digitalWrite(rm1, HIGH);
+      digitalWrite(lm2, LOW);      digitalWrite(rm2, LOW);
+
+    }
+
+
+    /*********For Backward *********/
+    else if (val == 'B')
+    {
+      Serial.println("BACK");
+      digitalWrite(lm2, HIGH);   digitalWrite(rm2, HIGH);
+      digitalWrite(lm1, LOW);  digitalWrite(rm1, LOW);
+
+    }
+    /*********Right*********/
+    else if (val == 'R')
+    {
+      Serial.println("RIGHT");
+      digitalWrite(lm1, HIGH);  digitalWrite(rm2, HIGH);
+      digitalWrite(lm2, LOW);       digitalWrite(rm1, LOW);
+    }
+    /*********Left*********/
+    else if (val == 'L')
+    {
+      Serial.println("LEFT");
+      digitalWrite(lm2, HIGH);  digitalWrite(rm1, HIGH);
+      digitalWrite(lm1, LOW);       digitalWrite(rm2, LOW);
+
+    }
+
+    /*********STOP*********/
+    else  {
+      Serial.println("stop!!!");
+      digitalWrite(lm1, LOW);      digitalWrite(rm1, LOW);
+      digitalWrite(lm2, LOW);       digitalWrite(rm2, LOW);
+
+    }
+
+  }
+}
+
+
+void sensorState() {
+  sensorData = analogRead(sensorPin);
+  if (sensorData >= mainThreshold) {
+    frizz = true;
+  }
+
+}
